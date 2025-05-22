a07dc888a28d0f17b6d701456eb904c133979845
eylles <ed.ylles1997@gmail.com>
2025-05-22 09:02:21 -0600

    Ensure keybinds and layout
    The user can add other keybinds and layout options but not disable the default ones

--- xterm-copyout.sh
+++ xterm-copyout.sh
@@ -5,6 +5,10 @@ config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/${dir_name}"
 config_file="${config_dir}/configrc"
 
 if [ -z "$xtdump" ] && [ -z "$choice" ]; then
+    # fzf layout
+    FZF_LAYOUT=""
+    # binds for fzf
+    FZF_BINDS=""
     # colorscheme for fzf, by default we ship dracula
     FZF_COLORS="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9,fg+:#f8f8f2,bg+:#44475a \
                 --color=hl+:#bd93f9,info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,marker:#ff79c6 \
@@ -17,9 +21,6 @@ if [ -z "$xtdump" ] && [ -z "$choice" ]; then
         . "$config_file"
     fi
 
-    FZF_LAYOUT="--border=rounded --layout=reverse --header-first"
-    FZF_BINDS="--bind alt-k:preview-up --bind alt-j:preview-down \
-               --bind='pgdn:half-page-down,pgup:half-page-up'"
     export FZF_DEFAULT_OPTS="${FZF_LAYOUT} ${FZF_COLORS} ${FZF_BINDS}"
 
     # the line(s) that will be copied to clipboard
@@ -42,7 +43,9 @@ if [ -z "$xtdump" ] && [ -z "$choice" ]; then
 else
         cat "$xtdump" | \
         fzf \
-        --height 100% \
+        --border=rounded --layout=reverse --header-first --height 100% \
+        --bind alt-k:preview-up --bind alt-j:preview-down \
+        --bind='pgdn:half-page-down,pgup:half-page-up' \
         --scrollbar='█' \
         --header "Xterm History" \
         --cycle --ansi -m | \
